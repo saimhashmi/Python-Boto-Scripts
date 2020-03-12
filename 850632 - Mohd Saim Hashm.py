e7178201941c613753fbbf8517dbbf510b1dbee6
saimhashmi <56545531+saimhashmi@users.noreply.github.com>
2020-03-12 09:56:26 +0530

    Add files via upload

---
+++ 850632 - Mohd Saim Hashm.py
@@ -0,0 +1,87 @@
+import argparse
+import os
+import time
+
+import googleapiclient.discovery
+from six.moves import input
+
+def list_instances(compute, project, zone):
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items'] if 'items' in result else None
+
+def create_instance(compute, project, zone, name, bucket):
+    image_response = compute.images().getFromFamily(project='debian-cloud', family='debian-9').execute()
+    source_disk_image = image_response['selfLink']
+
+    machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
+    startup_script = open(os.path.join(os.path.dirname(__file__), 'startup-script.sh'), 'r').read()
+    image_url = "http://storage.googleapis.com/gce-demo-input/photo.jpg"
+    image_caption = "Ready for dessert?"
+
+    config = {
+        'name': name,
+        'machineType': machine_type,
+        'disks': [{'boot': True,'autoDelete': True,'initializeParams': {'sourceImage': source_disk_image,}}],
+        'networkInterfaces': [{'network': 'global/networks/default','accessConfigs': [{'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}]}],
+        'serviceAccounts': [{'email': 'default','scopes': ['https://www.googleapis.com/auth/devstorage.read_write','https://www.googleapis.com/auth/logging.write']}],
+        'metadata': {'items': [{'key': 'startup-script','value': startup_script},
+                # Startup script is automatically executed by the
+                # instance upon startup.
+                 {'key': 'url','value': image_url}, 
+                 {'key': 'text','value': image_caption}, 
+                 {'key': 'bucket','value': bucket}]}}
+
+    return compute.instances().insert(
+        project=project, zone=zone, body=config).execute()
+
+def delete_instance(compute, project, zone, name):
+    return compute.instances().delete(project=project, zone=zone, instance=name).execute()
+
+def wait_for_operation(compute, project, zone, operation):
+    print('Waiting for operation to finish...')
+    while True:
+        result = compute.zoneOperations().get(project=project, zone=zone, operation=operation).execute()
+
+        if result['status'] == 'DONE':
+            print("done.")
+            if 'error' in result:
+                raise Exception(result['error'])
+            return result
+
+        time.sleep(1)
+
+def main(project, bucket, zone, instance_name, wait=True):
+    compute = googleapiclient.discovery.build('compute', 'v1')
+
+    print('Creating instance.')
+
+    operation = create_instance(compute, project, zone, instance_name, bucket)
+    wait_for_operation(compute, project, zone, operation['name'])
+
+    instances = list_instances(compute, project, zone)
+
+    print('Instances in project %s and zone %s:' % (project, zone))
+    for instance in instances:
+        print(' - ' + instance['name'])
+
+    print("""Instance created.\nIt will take a minute or two for the instance to complete work.\nCheck this URL: http://storage.googleapis.com/{}/output.png\nOnce the image is uploaded press enter to delete the instance.""".format(bucket))
+
+    if wait:
+        input()
+
+    print('Deleting instance.')
+
+    operation = delete_instance(compute, project, zone, instance_name)
+    wait_for_operation(compute, project, zone, operation['name'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('project_id', help='Your Google Cloud project ID.')
+    parser.add_argument('bucket_name', help='Your Google Cloud Storage bucket name.')
+    parser.add_argument('--zone', default='us-central1-f', help='Compute Engine zone to deploy to.')
+    parser.add_argument('--name', default='demo-instance', help='New instance name.')
+
+    args = parser.parse_args()
+
+    main(args.project_id, args.bucket_name, args.zone, args.name)
